95db570999f941d8b4bbba7a9db30537b84a4c8c
a1anwang <245121415@qq.com>
2018-01-11 17:01:08 +0800

    实现动态载入会话fragment
    聊天fragment 动态载入融云的会话fragment,来达到只和一个人聊的界面

--- OnlyTa/app/src/main/java/com/a1anwang/onlyta/ui/fragment/TalkFragment.java
+++ OnlyTa/app/src/main/java/com/a1anwang/onlyta/ui/fragment/TalkFragment.java
@@ -1,5 +1,7 @@
 package com.a1anwang.onlyta.ui.fragment;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,13 +10,16 @@ import com.a1anwang.onlyta.R;
 import com.a1anwang.onlyta.util.ToastUtils;
 
 import io.rong.imkit.RongIM;
+import io.rong.imkit.fragment.ConversationFragment;
+import io.rong.imlib.model.Conversation;
 
 /**
  * Created by a1anwang.com on 2018/1/2.
  */
 
 public  class TalkFragment extends BaseFragment{
-
+    private String mTargetId; //目标 Id
+    private Conversation.ConversationType mConversationType; //会话类型
 
     @Override
     public int setContentLayout() {
@@ -39,6 +44,22 @@ public  class TalkFragment extends BaseFragment{
     @Override
     public void afterInitView() {
 
+
+        mTargetId = "13262656236";
+
+        mConversationType = Conversation.ConversationType.PRIVATE;
+        /* 新建 ConversationFragment 实例，通过 setUri() 设置相关属性*/
+        ConversationFragment fragment = new ConversationFragment();
+        Uri uri = Uri.parse("rong://" + mContext. getApplicationInfo().packageName).buildUpon()
+                .appendPath("conversation").appendPath(mConversationType.getName().toLowerCase())
+                .appendQueryParameter("targetId", mTargetId).build();
+
+        fragment.setUri(uri);
+
+        /* 加载 ConversationFragment */
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.layout_content, fragment);
+        transaction.commit();
     }
 
     @Override
